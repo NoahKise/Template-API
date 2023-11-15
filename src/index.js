efb8a55ed299ecde82d6936df5fb075f9599cd43
noahkise <noah@kisefamily.com>
2023-11-15 11:18:25 -0800

    API calls working, Results saved as object, printing to DOM
    Co-authored-by: Gabriel Tucker <gabrieltucker260@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 import "bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./css/styles.css";
+import { getId } from "./practice";
+
+//UI LOGIC
+
+async function handleFormSubmission(e) {
+    e.preventDefault();
+    const gender = document.getElementById("gender").value;
+    const age = document.getElementById("age").value;
+    const identity = await getId(gender, age);
+    const p = document.createElement("p");
+    console.log(identity);
+    p.append(identity.result.name.first_name);
+    const body = document.querySelector("body");
+    body.append(p);
+}
+
+
+
+document.querySelector("form").addEventListener("submit", handleFormSubmission);
--- src/practice.js
+++ src/practice.js
@@ -1,20 +1,21 @@
-const gender = "male";
-const age = "33";
+export async function getId(gender, age) {
+    const apiKey = process.env.API_KEY;
+    const url = `https://random-identity-generator.p.rapidapi.com/identity?gender=${gender}&nameSet=american&country=us&minAge=${age}&maxAge=${age}`;
+    const options = {
+        method: 'GET',
+        headers: {
+            'X-RapidAPI-Key': apiKey,
+            'X-RapidAPI-Host': 'random-identity-generator.p.rapidapi.com'
+        }
+    };
 
-const url = `https://random-identity-generator.p.rapidapi.com/identity?gender=${gender}&nameSet=american&country=us&minAge=${age}&maxAge=${age}`;
-const options = {
-    method: 'GET',
-    headers: {
-        'X-RapidAPI-Key': process.env.API_KEY,
-        'X-RapidAPI-Host': 'random-identity-generator.p.rapidapi.com'
+    try {
+        const response = await fetch(url, options);
+        const resultText = await response.text();
+        const result = JSON.parse(resultText);
+        return result;
+    } catch (error) {
+        return error;
     }
-};
-
-try {
-    const response = fetch(url, options);
-    const result = response.text();
-    console.log(result);
-} catch (error) {
-    console.error(error);
 }
 
