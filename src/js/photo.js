e47a13bf11e55a18e4e07f9feee5258919b29b08
noahkise <noah@kisefamily.com>
2023-11-17 10:39:53 -0800

    template changes

--- src/js/photo.js
+++
@@ -1,7 +0,0 @@
-export async function getPhoto(gender) {
-    const url = `https://randomuser.me/api/?gender=${gender}`;
-    const response = await fetch(url);
-    const result = await response.json();
-    const picture = result.results[0].picture.large;
-    return picture;
-}
--- src/js/simpsons.js
+++
@@ -1,6 +0,0 @@
-export async function getQuote() {
-    const url = `https://thesimpsonsquoteapi.glitch.me/quotes`;
-    const response = await fetch(url);
-    const result = await response.json();
-    return result;
-}
